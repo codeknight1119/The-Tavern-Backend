17fb1e3b73d313a20ba8ed6aeb055b7fff79611a
codeknight1119 <code.knight1119@gmail.com>
2026-07-20 15:23:43 -0400

    update

--- services/firebase.js
+++ services/firebase.js
@@ -1,11 +1,9 @@
-const admin = require("firebase-admin");
-
-console.log(admin)
+const { initializeApp, cert } = require("firebase-admin/app");
 
 const serviceAccount = require("../credentials/serviceAccount.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+console.log(serviceAccount)
+initializeApp({
+    credential: cert(serviceAccount),
 });
 
 module.exports = admin;
