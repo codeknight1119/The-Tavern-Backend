f588a2ef3743901f18dfd1d11189a59413b5fb90
codeknight1119 <code.knight1119@gmail.com>
2026-08-11 17:39:35 -0400

    s

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ const server = http.createServer(async (req, res) => {
 
                 case "/checkMessage": {
                      const body = await readBody(req);
-                  const innapropiateContent = BANNEDWORDS.some(word => body.message.includes(keyword))
+                  const innapropiateContent = BANNEDWORDS.some(word => body.message.includes(word))
                     return sendJSON(res, {
                         clean: innapropiateContent
                     })
