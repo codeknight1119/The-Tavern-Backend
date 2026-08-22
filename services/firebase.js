524dac890faac10870829bba37badb5463ed14e0
codeknight1119 <code.knight1119@gmail.com>
2026-08-22 16:35:47 -0400

    test update

--- services/firebase.js
+++ services/firebase.js
@@ -2,7 +2,7 @@ const { initializeApp, cert } = require("firebase-admin/app");
 const { getAuth } = require("firebase-admin/auth");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("../credentials/serviceAccount.json");
+const serviceAccount = require("/etc/secrets/serviceAccount.json");
 
 initializeApp({
     credential: cert(serviceAccount),
